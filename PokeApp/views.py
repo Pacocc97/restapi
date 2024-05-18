9bda124061b4395f7119f9d62fff5c046268feb2
pacocc97@gmail.com <Pacocc97>
2024-05-18 10:40:38 -0600

    Remove comments

--- PokeApp/views.py
+++ PokeApp/views.py
@@ -20,12 +20,6 @@ def get_pokemon_list(request):
             return Response(status=status.HTTP_404_NOT_FOUND)
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    # pokemon_data = PokemonApiService.get_pokemon_list(limit,offset)
-    # if pokemon_data:
-    #     processed_data = PokemonApiService.process_pokemon_list(pokemon_data,limit, offset)
-    #     return Response(processed_data)
-    # else:
-    #     return Response(status=status.HTTP_404_NOT_FOUND)
       
 @api_view(['GET'])
 def get_pokemon_data(request, pokemon_name_or_id):
@@ -38,12 +32,6 @@ def get_pokemon_data(request, pokemon_name_or_id):
             return Response(status=status.HTTP_404_NOT_FOUND)
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    # pokemon_data = PokemonApiService.get_pokemon_data(pokemon_name_or_id)
-    # if pokemon_data:
-    #     processed_data = PokemonApiService.process_pokemon_data(pokemon_data)
-    #     return Response(processed_data)
-    # else:
-    #     return Response(status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['POST'])
 def create_pokemon(request):
@@ -56,12 +44,6 @@ def create_pokemon(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    # serializer = PokemonSerializer(data=request.data)
-    # if serializer.is_valid():
-    #     serializer.save()
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-    # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 @api_view(['PUT'])
 def update_pokemon(request, pk):
     try:
@@ -75,17 +57,6 @@ def update_pokemon(request, pk):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except ObjectDoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    # try:
-    #     pokemon = Pokemon.objects.get(pk=pk)
-    # except Pokemon.DoesNotExist:
-    #     return Response(status=status.HTTP_404_NOT_FOUND)
-    # request.data['updated_at'] = timezone.now()
-    # serializer = PokemonSerializer(pokemon, data=request.data)
-    # if serializer.is_valid():
-    #     serializer.save()
-    #     return Response(serializer.data)
-    # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 @api_view(['DELETE'])
 def delete_pokemon(request, pk):
     try:
@@ -94,13 +65,6 @@ def delete_pokemon(request, pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
     except ObjectDoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    # try:
-    #     pokemon = Pokemon.objects.get(pk=pk)
-    # except Pokemon.DoesNotExist:
-    #     return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    # pokemon.delete()
-    # return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET'])
 def get_detailed_pokemon(request, pk):
@@ -110,13 +74,6 @@ def get_detailed_pokemon(request, pk):
         return Response(serializer.data)
     except ObjectDoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    # try:
-    #     pokemon = Pokemon.objects.get(pk=pk)
-    # except Pokemon.DoesNotExist:
-    #     return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    # serializer = PokemonSerializer(pokemon)
-    # return Response(serializer.data)
 
 @api_view(['GET'])
 def get_all_pokemon(request):
@@ -131,14 +88,6 @@ def get_all_pokemon(request):
         return Response(response_data)
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_404_NOT_FOUND)
-    # pokemons = Pokemon.objects.all()
-    # total_score = sum(ScoreService.calculate_score(pokemon) for pokemon in pokemons)
-    # serializer = PokemonSerializer(pokemons, many=True)
-    # response_data = {
-    #     "score": total_score,
-    #     "results": serializer.data
-    # }
-    # return Response(response_data)
 
 @api_view(['GET'])
 def get_pokemon_score(request, pk):
@@ -148,10 +97,3 @@ def get_pokemon_score(request, pk):
         return Response({"score": score})
     except ObjectDoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    # try:
-    #     pokemon = Pokemon.objects.get(pk=pk)
-    # except Pokemon.DoesNotExist:
-    #     return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    # score = ScoreService.calculate_score(pokemon)
-    # return Response({"score": score})
